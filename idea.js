1f9620a5f013aec18578b049383d4c6885bead36
Louis Acresti <louis.acresti@gmail.com>
2011-01-29 18:33:38 -0500

    - Cleaning up idea.js

--- idea.js
+++ idea.js
@@ -11,9 +11,6 @@ var passwords = {
 var validClients = [];
 
 mocket.on('$connection', function (client) {
-    for (var i = 0; i < log.length; ++i) {
-    }
-
     client.on('login', function (name, password) {
         if (!(name in passwords) || passwords[name] !== password) {
             client.send('authenticationFailed');
